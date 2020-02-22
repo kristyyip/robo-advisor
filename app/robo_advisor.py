7f5cdb79d57f7453d5587cf7215f1db0a6016a1c
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-02-22 14:18:16 -0500

    Sending alerts via email

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -12,6 +12,9 @@ load_dotenv()
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 # utility function to convert float or integer to usd-formatted string (for printing)
 # ... adapted from: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766
 def to_usd(my_price):
@@ -22,7 +25,7 @@ def to_usd(my_price):
 #
 
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-symbol = input("Please choose a stock symbol to evaluate: ") #accept user input
+symbol = input("Please choose a valid stock symbol to evaluate: ") #accept user input
 length = len(symbol)
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
@@ -62,12 +65,98 @@ else:
     recent_high = max(high_prices)
     recent_low = min(low_prices)
 
+    # sending alerts via email 
+    current_datetime = datetime.now() # source: tecadmin.net/get-current-date-time-python/
+
+    while True:
+        email_choice = input("Would you like to receive an email alerting you about price movements? (Yes or No): ")
+
+        second_latest_close = parsed_response["Time Series (Daily)"][dates[1]]["4. close"]
+        second_latest_day = list(tsd.keys())[1]
+        timestamp = current_datetime.strftime("%B %d, %Y at %I:%M %p")
+
+        if email_choice.lower() == "yes":
+            print("Great! You'll receive an email if there was a price movement within the past day of the latest day's stock.")
+
+            if (float(latest_close) < .95*(float(second_latest_close))):
+                SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+                MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+
+                client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+                print("CLIENT:", type(client))
+
+                subject = f"PRICE MOVEMENT ALERT FOR {symbol}!"
+
+                # body of the email
+                html_content = f"""
+                    <h3>PRICE DECREASE FOR {symbol}!</h3>
+                    <p>Date: {timestamp}</p>
+                    <p>The price has decreased by more than 5% within the past day of the latest day's stock. The closing price for {second_latest_day} was {second_latest_close}, decreasing to {latest_close} for {last_refreshed}.
+                    <h3>Thank you for using Robo Advisor.</h3>
+                """
+
+                print("HTML:", html_content)
+
+                message = Mail(from_email=MY_ADDRESS, to_emails=MY_ADDRESS, subject=subject, html_content=html_content)
+
+                try:
+                    response = client.send(message)
+
+                    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+                    print(response.status_code) #> 202 indicates SUCCESS
+                    print(response.body)
+                    print(response.headers)
+
+                except Exception as e:
+                    print("OOPS", e.message)
+                
+                break
+            elif (float(latest_close) > 1.05*(float(second_latest_close))):
+                SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+                MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+
+                client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+                print("CLIENT:", type(client))
+
+                subject = f"PRICE MOVEMENT ALERT FOR {symbol}!"
+
+                # body of the email
+                html_content = f"""
+                    <h3>PRICE INCREASE FOR {symbol}!</h3>
+                    <p>Date: {timestamp}</p>
+                    <p>The price has increased by more than 5% within the past day of the latest day's stock. The closing price for {second_latest_day} was {second_latest_close}, increasing to {latest_close} for {last_refreshed}.
+                    <h3>Thank you for using Robo Advisor.</h3>
+                """
+
+                print("HTML:", html_content)
+
+                message = Mail(from_email=MY_ADDRESS, to_emails=MY_ADDRESS, subject=subject, html_content=html_content)
+
+                try:
+                    response = client.send(message)
+
+                    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+                    print(response.status_code) #> 202 indicates SUCCESS
+                    print(response.body)
+                    print(response.headers)
+
+                except Exception as e:
+                    print("OOPS", e.message)
+                
+                break
+            else:
+                break
+        elif email_choice.lower() == "no":
+            break
+        else:
+            print("Sorry, that isn't a valid choice. Please choose 'Yes' or 'No'.")
+
     #
     # INFO OUTPUTS
     #
 
     # csv_file_path = "data/prices.csv"
-
+    ## creating a csv file of the stock prices
     csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
     csv_headers = ["timestamp", "opening", "high", "low", "closing", "volume"]
@@ -87,15 +176,12 @@ else:
                 "volume": daily_prices["5. volume"]
             })
 
-
     print("-------------------------")
     print("SELECTED SYMBOL: " + symbol)
     print("-------------------------")
     print("REQUESTING STOCK MARKET DATA...")
 
     ## print current date and time
-    ## source: tecadmin.net/get-current-date-time-python/
-    current_datetime = datetime.now()
     print("REQUEST AT: " + current_datetime.strftime("%Y-%m-%d %I:%M %p"))
     print("-------------------------")
     print(f"LATEST DAY: {last_refreshed}")
@@ -104,7 +190,7 @@ else:
     print(f"RECENT LOW: {to_usd(float(recent_low))}")
     print("-------------------------")
     
-    # recommendation algorithm
+    ## recommendation algorithm
     if (float(latest_close) >= recent_low) and (float(latest_close) < 1.2*recent_low):
         print("RECOMMENDATION: BUY!")
         print("RECOMMENDATION REASON: BECAUSE THE STOCK'S LATEST CLOSE IS LESS THAN 20% ABOVE ITS RECENT LOW")
@@ -114,8 +200,11 @@ else:
     print("-------------------------")
     print(f"WRITING DATA TO CSV: {csv_file_path}...")
     print("-------------------------")
+    print("GENERATING LINE GRAPHS...")
+    print("-------------------------")
     print("HAPPY INVESTING!")
     print("-------------------------")
+
 
     # creating four line graphs showing the opening, high, low, and closing stock prices over time
     ## source: https://stackoverflow.com/questions/56179109/plot-stock-data-from-csv-file-not-showing-date-correctly
